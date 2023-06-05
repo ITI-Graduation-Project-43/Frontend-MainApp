6674551330fdad5d7e18e8020b970bf1a08c6b63
Mahmoud Mohamed Ali ALi <99610976+iMahmouddAli@users.noreply.github.com>
2023-06-05 18:13:55 +0300

    aa

--- src/app/About/about.module.ts
+++ src/app/About/about.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmptyComponent } from './Components/empty/empty.component';
 import { MainSectionComponent } from './Components/main-section/main-section.component';
+import { AboutComponent } from './about.component';
 
 @NgModule({
-  declarations: [EmptyComponent, MainSectionComponent],
+  declarations: [AboutComponent, EmptyComponent, MainSectionComponent],
   imports: [CommonModule],
   exports: [MainSectionComponent],
 })

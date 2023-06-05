904ce472578a6679f54f55191d1b5f5b5c1e72b7
Mahmoud Mohamed Ali ALi <99610976+iMahmouddAli@users.noreply.github.com>
2023-06-05 20:56:19 +0300

    adding vision section

--- src/app/About/about.module.ts
+++ src/app/About/about.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about.component';
 import { EmptyComponent } from './Components/empty/empty.component';
 import { MainSectionComponent } from './Components/main-section/main-section.component';
+import { VisionComponent } from './Components/vision/vision.component';
 
 @NgModule({
-  declarations: [AboutComponent, EmptyComponent, MainSectionComponent],
+  declarations: [AboutComponent, EmptyComponent, MainSectionComponent, VisionComponent],
   imports: [CommonModule],
   exports: [MainSectionComponent],
 })

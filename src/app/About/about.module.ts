3dde22f4ae8926dbe88ab2d036bec754a6a18132
Tarek Eslam <89143141+tarek1o@users.noreply.github.com>
2023-06-05 17:50:13 +0300

    .

--- src/app/About/about.module.ts
+++ src/app/About/about.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmptyComponent } from './Components/empty/empty.component';
+import { Empty2Component } from './Components/empty2/empty2.component';
 
 
 
 @NgModule({
   declarations: [
-    EmptyComponent
+    EmptyComponent,
+    Empty2Component
   ],
   imports: [
     CommonModule

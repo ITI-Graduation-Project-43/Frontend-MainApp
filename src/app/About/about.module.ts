a78fbc95bd442f6513ff29a3c9fcf14599ee1104
MoIbrahim10 <mielbedwihy@std.mans.edu.eg>
2023-06-05 20:09:03 +0300

    added student also bought card

--- src/app/About/about.module.ts
+++ src/app/About/about.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { AboutComponent } from './about.component';
 import { EmptyComponent } from './Components/empty/empty.component';
 import { MainSectionComponent } from './Components/main-section/main-section.component';
 
 @NgModule({
-  declarations: [AboutComponent, EmptyComponent, MainSectionComponent],
+  declarations: [EmptyComponent, AboutComponent, MainSectionComponent],
   imports: [CommonModule],
   exports: [MainSectionComponent],
 })

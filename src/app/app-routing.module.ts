9d27465b9b957cbd8ff8c8224989f68e836a7872
Mahmoud Mohamed Ali ALi <99610976+iMahmouddAli@users.noreply.github.com>
2023-06-05 05:27:20 +0300

    first in about module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './Core/header/header.component';
 import { CommonModule } from '@angular/common';
+import { AboutComponent } from './About/about.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: 'header', pathMatch: 'full' },
-  {path: "header", component: HeaderComponent},
-]
+  { path: '', redirectTo: 'header', pathMatch: 'full' },
+  { path: 'header', component: HeaderComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [CommonModule, RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

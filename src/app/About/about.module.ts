adca854b78a3111dc6e6e9f6798271736bb5d9c2
MoIbrahim10 <mielbedwihy@std.mans.edu.eg>
2023-06-05 16:50:54 +0300

    fixed about component error

--- src/app/About/about.module.ts
+++ src/app/About/about.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AboutComponent } from './about.component';
 import { EmptyComponent } from './Components/empty/empty.component';
 import { MainSectionComponent } from './Components/main-section/main-section.component';
 
 @NgModule({
-  declarations: [EmptyComponent, MainSectionComponent],
+  declarations: [AboutComponent, EmptyComponent, MainSectionComponent],
   imports: [CommonModule],
   exports: [MainSectionComponent],
 })

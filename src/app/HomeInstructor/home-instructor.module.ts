171bf6f01a144e735e4d63b1a2156fd3df371b11
Mahmoud Mohamed Ali ALi <99610976+iMahmouddAli@users.noreply.github.com>
2023-06-07 13:12:28 +0300

    new test

--- src/app/HomeInstructor/home-instructor.module.ts
+++ src/app/HomeInstructor/home-instructor.module.ts
@@ -3,14 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomeInstructorComponent } from './home-instructor.component';
 import { HeroSectionComponent } from './Components/hero-section/hero-section.component';
 import { GetStartedComponent } from '../About/Components/get-started/get-started.component';
-
+import { AboutModule } from '../About/about.module';
 
 @NgModule({
-  declarations: [
-    HomeInstructorComponent,
-    HeroSectionComponent,
-    GetStartedComponent,
-  ],
-  imports: [CommonModule],
+  declarations: [HomeInstructorComponent, HeroSectionComponent],
+  imports: [CommonModule, AboutModule],
 })
 export class HomeInstructorModule {}

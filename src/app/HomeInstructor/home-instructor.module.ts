8cada93dba966f0e480b3dab814fc1119a948a73
Mahmoud Mohamed Ali ALi <99610976+iMahmouddAli@users.noreply.github.com>
2023-06-07 14:02:16 +0300

    adding requirments to home instructors

--- src/app/HomeInstructor/home-instructor.module.ts
+++ src/app/HomeInstructor/home-instructor.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HomeInstructorComponent } from './home-instructor.component';
 import { HeroSectionComponent } from './Components/hero-section/hero-section.component';
 import { AboutModule } from '../About/about.module';
+import { RequirementsComponent } from './Components/requirements/requirements.component';
 
 
 @NgModule({
   declarations: [
     HomeInstructorComponent,
     HeroSectionComponent,
+    RequirementsComponent,
   ],
   imports: [CommonModule,AboutModule],
 })

4e8cf7f483ef7220867d1bdd0a19c9e02e3d42cf
MoIbrahim10 <mielbedwihy@std.mans.edu.eg>
2023-06-08 21:14:11 +0300

    updated course content section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './Core/header/header.component';
 import { CommonModule } from '@angular/common';
 import { CourseModule } from './Course/course.module';
-import { CourseComponent } from './Course/course.component';
-import { AboutComponent } from './About/about.component';
 import { CategoryComponent } from './Category/category.component';
-import { HomeInstructorComponent } from './HomeInstructor/home-instructor.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'header', pathMatch: 'full' },

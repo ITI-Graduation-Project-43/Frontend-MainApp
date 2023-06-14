d3878eef3c78e312ccfc9f8f13d9cbf401118d58
MoIbrahim10 <mielbedwihy@std.mans.edu.eg>
2023-06-14 17:41:55 +0300

    styled quiz/article

--- src/app/CourseLesson/CourseLesson.module.ts
+++ src/app/CourseLesson/CourseLesson.module.ts
@@ -5,14 +5,16 @@ import { FormsModule } from '@angular/forms';
 
 import { SharedModule } from '../Shared/SharedModule/SharedModule.module';
 import { CourseLessonComponent } from './CourseLesson.component';
+import { VideoPlayerComponent } from './video-player/video-player/video-player.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
     FormsModule,
+
     RouterModule.forChild([{ path: '', component: CourseLessonComponent }]),
   ],
-  declarations: [CourseLessonComponent],
+  declarations: [CourseLessonComponent, VideoPlayerComponent],
 })
 export class CourseLessonModule {}

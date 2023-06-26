934a75475385fa30c687f30d523296777bfecd78
Mahmoud Mohamed Ali ALi <99610976+iMahmouddAli@users.noreply.github.com>
2023-06-26 05:36:40 +0300

    edti instructorID in createCourse and editCourse

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  APIURL: 'http://localhost:8855/api/', // IIS
-  // APIURL: 'https://localhost:7129/api/',
-  secretKey: 'Messi'
+  // APIURL: 'http://localhost:8855/api/', // IIS
+  APIURL: 'https://localhost:7129/api/',
+  secretKey: 'Messi',
 };
